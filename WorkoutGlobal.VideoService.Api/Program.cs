604a4841c3d0412e23fe9f22e95c8bfaab2f2ffa
MagzyCode <mihailkazarevich@mail.ru>
2022-11-09 11:11:34 +0300

    .

--- WorkoutGlobal.VideoService.Api/Program.cs
+++ WorkoutGlobal.VideoService.Api/Program.cs
@@ -1,9 +1,36 @@
+<<<<<<< Updated upstream
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+=======
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutGlobal.VideoService.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+// using deprecated AddFluentValidation method for auto validation
+builder.Services.AddFluentValidationAutoValidation()
+    .AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddControllers();
+//builder.Services.AddControllers()
+//    .AddFluentValidation(configuration =>
+//    {
+//        configuration.RegisterValidatorsFromAssemblyContaining<Program>();
+//        configuration.DisableDataAnnotationsValidation = true;
+//    });
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.ConfigureAttributes();
+>>>>>>> Stashed changes
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
